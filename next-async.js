4c620384eaabf2a0cd6a178fb4674700fd5e5b75
U0 <rhyzix@gmail.com>
2012-12-15 18:17:40 +0800

    universal ultimate version update:bomb:

--- next-async.js
+++ next-async.js
@@ -7,46 +7,46 @@
  * Licensed under the MIT license
  *
  * @example :
- *		next(function(v, next) {
- *			console.log(v); //log 1
- *			next(2, 3);
- *		}).next(function(a, b, next) {
- *			console.log(a+b); //log 5
- *			ajax(next); //set ajax callback
- *		}).next(function(data, next) {
- *			//do something...
- *		})(1);//start
+ *      next(function(v, next) {
+ *          console.log(v); //log 1
+ *          next(2, 3);
+ *      }).next(function(a, b, next) {
+ *          console.log(a+b); //log 5
+ *          ajax(next); //set ajax callback
+ *      }).next(function(data, next) {
+ *          //do something...
+ *      })(1);//start
  */
 ;(function ($, undefined) {
-	"use strict";
-	
-	//bind global
-	$.next = next;
+    "use strict";
 
-	//main
-	function next(fn) {
-		var first = createNext(fn)
-		  , last  = first;
+    //bind global
+    $.next = next;
 
-		function executor() {
-			first.apply(this, arguments);
-		}
+    //main
+    function next(fn) {
+        var first = createNext(fn)
+          , last  = first;
 
-		//only expose executor
-		//neither midify nor expose fisrt
-		executor.next = function (fn) {
-			last.next = last = createNext(fn);
-			return this;
-		};
+        function executor() {
+            first.apply(this, arguments);
+        }
 
-		return executor;
-	}
+        //only expose executor
+        //neither midify nor expose fisrt
+        executor.next = function (fn) {
+            last.next = last = createNext(fn);
+            return this;
+        };
 
-	//ceate a new Next object(also is function)
-	function createNext(fn) {
-		return function Next() {
-			Array.prototype.push.call(arguments, Next.next);
-			fn.apply(this, arguments);
-		};
-	}
+        return executor;
+    }
+
+    //ceate a new Next object(also is function)
+    function createNext(fn) {
+        return function Next() {
+            Array.prototype.push.call(arguments, Next.next);
+            fn.apply(this, arguments);
+        };
+    }
 })(window.jQuery || window);
